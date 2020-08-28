e90398c3afeacee635a228d6eddc562a80605de6
SaatwikRishi <saatwiksanjay@gmail.com>
2020-08-28 23:46:31 +0530

    Update user_data.dart
    pushin

--- lib/providers/user_data.dart
+++ lib/providers/user_data.dart
@@ -1,8 +1,10 @@
 class Contacts {
-  String id;
-  String name;
-  String type;
-  String phone;
+  final String avatar;
+  final String id;
+  final String name;
+  final String type;
+  final String phone;
+  Contacts({this.id, this.avatar, this.name, this.type, this.phone});
 }
 
 class User {
@@ -10,4 +12,26 @@ class User {
   String phone;
   String address;
   User _info;
+  List<Contacts> _contacts = [
+    Contacts(
+        id: '1',
+        avatar: null,
+        name: 'BokChoda',
+        type: 'friend',
+        phone: '+9199999333'),
+    Contacts(
+        id: '2',
+        avatar: null,
+        name: 'Non A BOKA',
+        type: 'friend',
+        phone: '+99'),
+    Contacts(id: '3', avatar: null, name: 'Polizz', type: 'auth', phone: '100'),
+    Contacts(id: '4', avatar: null, name: 'Amb', type: 'auth', phone: '101'),
+    Contacts(
+        id: '5',
+        avatar: null,
+        name: 'Saatwik DADA',
+        type: 'friend',
+        phone: '+12345678')
+  ];
 }
