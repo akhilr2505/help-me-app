3a435492b4f238bbc1b891ffcc470fcd6d14a85f
Yash Tripathi <Drkakku19@gmail.com>
2020-08-29 00:54:31 +0530

    change

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -10,28 +10,6 @@ class HomeScreen extends StatelessWidget {
       ),
       drawer: HomeDrawer(),
       body: Center(
-
-        child: Expanded(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text("HELP ME NOW", style: TextStyle(fontSize: 37)),
-                RawMaterialButton(
-                  fillColor: Colors.red,
-                  onPressed: () {},
-                  elevation: 5,
-                  child: Text(
-                    "SOS",
-                    style: TextStyle(fontSize: 55),
-                  ),
-                  padding:
-                      EdgeInsets.all(MediaQuery.of(context).size.width * 0.37),
-                  materialTapTargetSize: MaterialTapTargetSize.padded,
-                  shape: CircleBorder(),
-                ),
-              ]),
-        ),
-
         child:
             Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
           Text("HELP ME NOW", style: TextStyle(fontSize: 37)),
@@ -48,7 +26,6 @@ class HomeScreen extends StatelessWidget {
             shape: CircleBorder(),
           ),
         ]),
-
       ),
     );
   }
